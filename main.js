b1b6f00d01172857f2933a6b8174a8cf5cb8ac98
Moraes <wmarcono@globo.com>
2020-02-10 14:13:16 -0300

    evitar que a cobrinha saia da tela

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ snake[0] = {
     x: 8 * box, y: 8 * box  // coordenadas iniciando no meio da tela
 }
 let direction = "right";
-let jogo = setInterval(iniciarJogo, 1000); // seta o tempo de atualização(ação da cobra)
 
 function criarBG(){  //metodo que cria a tela
     context.fillStyle = "lightgreen"; 
@@ -20,17 +19,31 @@ function criarCobrinha(){ //metodo de criação da cobrinha
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "right") snake += box;
-    if(direction == "left") snake += box;
-    if(direction == "up") snake += box;
-    if(direction == "down") snake == box;
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -41,3 +54,5 @@ function iniciarJogo(){
 
     snake.unshift(newHead);
 }
+
+let jogo = setInterval(iniciarJogo, 100); // seta o tempo de atualização(ação da cobra)
